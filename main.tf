d069c367ccdf80735a27f2489acfa637de45b17c
Darshan Nikam <165551512+darsh-86@users.noreply.github.com>
2024-06-05 19:31:54 +0530

    n

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "wordpress-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["your-admin-ip/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
