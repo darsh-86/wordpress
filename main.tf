8dd2594f92893c6dea389eb1cf59840a92f8edbb
Darshan Nikam <165551512+darsh-86@users.noreply.github.com>
2024-06-06 16:00:15 +0530

    n

--- main.tf
+++ main.tf
@@ -2,24 +2,6 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_instance" "wordpress" {
-  ami           = "ami-069195b1b84c1a70f" # Amazon Linux 2 AMI
-  instance_type = "t2.medium"
-  key_name      = "parisIAM"
-
-  security_groups = ["wordpress-sg"]
-}
-/*user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y docker
-              service docker start
-              usermod -a -G docker ec2-user
-              docker run -d --name wordpress -p 80:80 -p 443:443 -v /efs/wordpress:/var/www/html wordpress
-              docker run -d --name nginx -p 8080:80 --link wordpress:wordpress nginx
-              EOF 
-} */
-
 resource "aws_security_group" "wordpress-sg" {
   name = "wordpress-sg"
 
@@ -44,6 +26,13 @@ resource "aws_security_group" "wordpress-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -52,7 +41,7 @@ resource "aws_security_group" "wordpress-sg" {
   }
 }
 
-/* resource "aws_db_instance" "wordpress_db" {
+resource "aws_db_instance" "wordpress_db" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
@@ -63,4 +52,28 @@ resource "aws_security_group" "wordpress-sg" {
   password             = "admin4321"
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
-}*/
+}
+
+resource "aws_instance" "wordpress" {
+  ami           = "ami-069195b1b84c1a70f" # Amazon Linux 2 AMI
+  instance_type = "t2.medium"
+  key_name      = "parisIAM"
+
+  security_groups = [aws_security_group.wordpress-sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo -i
+              yum update -y
+              yum install -y docker
+              systemctl enable --now docker
+              docker network create wordpress-network
+
+              docker run -d --name wordpress --network wordpress-network \\
+                -e WORDPRESS_DB_HOST=${aws_db_instance.wordpress_db.endpoint}:3306 \\
+                -e WORDPRESS_DB_USER=admin \\
+                -e WORDPRESS_DB_PASSWORD=admin4321 \\
+                -e WORDPRESS_DB_NAME=wordpress \\
+                -p 8080:8080 wordpress
+                EOF 
+}
