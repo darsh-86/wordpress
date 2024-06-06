d2a45ec9fd036c106f1f4ac7cd52c474229abf09
Darshan Nikam <165551512+darsh-86@users.noreply.github.com>
2024-06-06 16:41:21 +0530

    n

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_db_instance" "wordpress_db" {
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "8.0"
-  instance_class       = "db.t2.micro"
+  instance_class       = "db.t3.micro"
   db_name                 = "wordpressdb"
   username             = "admin"
   password             = "admin4321"
